8f9f2ed75a87926d9024d6fc031d24c6d4302757
Dries_DS <dries.ds@hotmail.com>
2015-11-06 16:39:32 +0100

    r8mat_print_some added

--- src/dgesvdtest.f90
+++ src/dgesvdtest.f90
@@ -276,4 +276,168 @@ subroutine r8vec_print ( n, a, title )
   return
 end subroutine
 
+subroutine r8vec_print_some ( n, a, i_lo, i_hi, title )
+
+!*****************************************************************************80
+!
+!! R8VEC_PRINT_SOME prints "some" of an R8VEC.
+!
+!  Discussion:
+!
+!    An R8VEC is a vector of R8 values.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    16 October 2006
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer N, the number of entries of the vector.
+!
+!    Input, real ( kind = 8 ) A(N), the vector to be printed.
+!
+!    Input, integer I_LO, I_HI, the first and last indices to print.
+!    The routine expects 1 <= I_LO <= I_HI <= N.
+!
+!    Input, character ( len = * ) TITLE, an optional title.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(n)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) i_hi
+  integer ( kind = 4 ) i_lo
+  character ( len = * ) title
+
+  if ( 0 < len_trim ( title ) ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) trim ( title )
+  end if
+
+  write ( *, '(a)' ) ' '
+  do i = max ( i_lo, 1 ), min ( i_hi, n )
+    write ( *, '(2x,i8,2x,g14.8)' ) i, a(i)
+  end do
+
+  return
+end subroutine
+
+subroutine r8mat_print_some ( m, n, a, ilo, jlo, ihi, jhi, title )
+
+!*****************************************************************************80
+!
+!! R8MAT_PRINT_SOME prints some of an R8MAT.
+!
+!  Discussion:
+!
+!    An R8MAT is a two dimensional matrix of double precision real values.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    26 March 2005
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer M, N, the number of rows and columns.
+!
+!    Input, real ( kind = 8 ) A(M,N), an M by N matrix to be printed.
+!
+!    Input, integer ILO, JLO, the first row and column to print.
+!
+!    Input, integer IHI, JHI, the last row and column to print.
+!
+!    Input, character ( len = * ) TITLE, an optional title.
+!
+  implicit none
+
+  integer ( kind = 4 ), parameter :: incx = 5
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(m,n)
+  character ( len = 14 ) ctemp(incx)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) i2hi
+  integer ( kind = 4 ) i2lo
+  integer ( kind = 4 ) ihi
+  integer ( kind = 4 ) ilo
+  integer ( kind = 4 ) inc
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) j2
+  integer ( kind = 4 ) j2hi
+  integer ( kind = 4 ) j2lo
+  integer ( kind = 4 ) jhi
+  integer ( kind = 4 ) jlo
+  character ( len = * ) title
+
+  if ( 0 < len_trim ( title ) ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) trim ( title )
+  end if
+
+  do j2lo = max ( jlo, 1 ), min ( jhi, n ), incx
+
+    j2hi = j2lo + incx - 1
+    j2hi = min ( j2hi, n )
+    j2hi = min ( j2hi, jhi )
+
+    inc = j2hi + 1 - j2lo
+
+    write ( *, '(a)' ) ' '
+
+    do j = j2lo, j2hi
+      j2 = j + 1 - j2lo
+      write ( ctemp(j2), '(i8,6x)' ) j
+    end do
+
+    write ( *, '(''  Col   '',5a14)' ) ctemp(1:inc)
+    write ( *, '(a)' ) '  Row'
+    write ( *, '(a)' ) ' '
+
+    i2lo = max ( ilo, 1 )
+    i2hi = min ( ihi, m )
+
+    do i = i2lo, i2hi
+
+      do j2 = 1, inc
+
+        j = j2lo - 1 + j2
+
+        if ( a(i,j) == real ( int ( a(i,j) ), kind = 8 ) ) then
+          write ( ctemp(j2), '(f8.0,6x)' ) a(i,j)
+        else
+          write ( ctemp(j2), '(g14.6)' ) a(i,j)
+        end if
+
+      end do
+
+      write ( *, '(i5,1x,5a14)' ) i, ( ctemp(j), j = 1, inc )
+
+    end do
+
+  end do
+
+  write ( *, '(a)' ) ' '
+
+  return
+end subroutine
+
 end program
