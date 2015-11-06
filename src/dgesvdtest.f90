8e8fea93c806e83bd3ce60742ddc7269b37c7dc7
Dries_DS <dries.ds@hotmail.com>
2015-11-06 16:35:45 +0100

    added subroutine r8mat_print

--- src/dgesvdtest.f90
+++ src/dgesvdtest.f90
@@ -100,4 +100,180 @@ subroutine test07 ( )
   return
 end subroutine
 
+subroutine r8mat_print ( m, n, a, title )
+
+!*****************************************************************************80
+!
+!! R8MAT_PRINT prints an R8MAT.
+!
+!  Discussion:
+!
+!    An R8MAT is a two dimensional matrix of double precision real values.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    12 September 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer M, the number of rows in A.
+!
+!    Input, integer N, the number of columns in A.
+!
+!    Input, real ( kind = 8 ) A(M,N), the matrix.
+!
+!    Input, character ( len = * ) TITLE, a title to be printed.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(m,n)
+  character ( len = * ) title
+
+  call r8mat_print_some ( m, n, a, 1, 1, m, n, title )
+
+  return
+end subroutine
+
+subroutine r8mat_uniform_01 ( m, n, seed, r )
+
+!*****************************************************************************80
+!
+!! R8MAT_UNIFORM_01 fills an R8MAT with unit pseudorandom numbers.
+!
+!  Discussion:
+!
+!    An R8MAT is a two dimensional matrix of double precision real values.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    11 August 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Reference:
+!
+!    Paul Bratley, Bennett Fox, Linus Schrage,
+!    A Guide to Simulation,
+!    Springer Verlag, pages 201-202, 1983.
+!
+!    Bennett Fox,
+!    Algorithm 647:
+!    Implementation and Relative Efficiency of Quasirandom
+!    Sequence Generators,
+!    ACM Transactions on Mathematical Software,
+!    Volume 12, Number 4, pages 362-376, 1986.
+!
+!    Peter Lewis, Allen Goodman, James Miller,
+!    A Pseudo-Random Number Generator for the System/360,
+!    IBM Systems Journal,
+!    Volume 8, pages 136-143, 1969.
+!
+!  Parameters:
+!
+!    Input, integer M, N, the number of rows and columns in the array.
+!
+!    Input/output, integer SEED, the "seed" value, which should NOT be 0.
+!    On output, SEED has been updated.
+!
+!    Output, real ( kind = 8 ) R(M,N), the array of pseudorandom values.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) k
+  integer ( kind = 4 ) seed
+  real ( kind = 8 ) r(m,n)
+
+  do j = 1, n
+
+    do i = 1, m
+
+      k = seed / 127773
+
+      seed = 16807 * ( seed - k * 127773 ) - k * 2836
+
+      if ( seed < 0 ) then
+        seed = seed + huge ( seed )
+      end if
+
+      r(i,j) = real ( seed, kind = 8 ) * 4.656612875D-10
+
+    end do
+  end do
+
+  return
+end subroutine
+
+subroutine r8vec_print ( n, a, title )
+
+!*****************************************************************************80
+!
+!! R8VEC_PRINT prints an R8VEC.
+!
+!  Discussion:
+!
+!    An R8VEC is an array of double precision real values.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    22 August 2000
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer N, the number of components of the vector.
+!
+!    Input, real ( kind = 8 ) A(N), the vector to be printed.
+!
+!    Input, character ( len = * ) TITLE, an optional title.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(n)
+  integer ( kind = 4 ) i
+  character ( len = * ) title
+
+  if ( 0 < len_trim ( title ) ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) trim ( title )
+  end if
+
+  write ( *, '(a)' ) ' '
+  do i = 1, n
+    write ( *, '(2x,i8,2x,g16.8)' ) i, a(i)
+  end do
+
+  return
+end subroutine
+
 end program
