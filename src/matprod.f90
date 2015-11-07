8e2a9a3cd8fd31fea9557654b1ef8408befbfd63
Dries_DS <dries.ds@hotmail.com>
2015-11-07 22:13:25 +0100

    matprod for every possibility

--- src/matprod.f90
+++ src/matprod.f90
@@ -17,6 +17,12 @@ contains
 		if (A%full) then
 			!write(*,*) 'going to leftfullprod()'
 			call leftfullprod(A, B)
+		else
+			if (B%full) then
+				call rankfullprod(A,B)
+			else
+				call rankrankprod(A,B)
+			endif
 		endif
 
 		deallocate(A)
@@ -25,7 +31,7 @@ contains
 	
 	subroutine leftfullprod(A, B)
 		! A*B = C - met A van volle rang dus
-		! aangezien we de getransponeerden opslaan:
+		! aangezien we de getransponeerden opslaan: (C^t = C%Vt^t*C%Ut)
 		! met B niet van volle rank: C^t = (A*B)^t = B^t*A^t = (B%Ut^t*B%Vt)^t*A%Ut = B%Vt^t*B%Ut*A%Ut
 		! met B van volle rank: C^t = (A*B)^t = B^t*A^t = B%Ut*A%Ut
 		! in beide gevallen moet enkel het product B%Ut*A%Ut berekend worden
@@ -56,10 +62,76 @@ contains
 
 	end subroutine
 	
-	subroutine rankfullprod()
+	subroutine rankfullprod(A, B)
+		! A*B = C - met A niet van volle rank en B wel
+		! aangezien we de getransponeerden opslaan: (C^t = C%Vt^t*C%Ut)
+		! C^t = (A*B)^t = B^t*A^t = B%Ut*(A%Ut^t*A%Vt)^t = B%Ut*A%Vt^t*A%Ut
+		! we berekenen dus C%Ut = A%Ut en C%Vt = B%Ut*A%Vt^t
+
+		type(Matrix), pointer :: A, B, C
+		double precision :: alpha, beta
+		integer :: m,n,k
+		
+		m = size(B%Ut,1)
+		n = size(A%Vt,1)
+		k = size(B%Ut,2)
+		alpha = 1.0
+		beta = 0.0
+
+		allocate(C)
+		C%full = .false.
+		allocate(C%Ut(size(A%Ut,1),size(A%Ut,2)))
+		C%Ut = A%Ut
+		allocate(C%Vt(m,n))
+		
+		!write(*,*) 'DEBUGGING INFORMATION'
+		!write(*,*) 'size of A%Ut: ', size(A%Ut,1), ',', size(A%Ut,2)
+		!write(*,*) 'size of B%Ut: ', size(B%Ut,1), ',', size(B%Ut,2)
+		
+		call dgemm('N','T', m, n, k, alpha, B%Ut, m, A%Vt, n, beta, C%Ut, m)
+
+		call matrixWriter(C)
+		
+		deallocate(C)
+
 	end subroutine
 	
-	subroutine rankrankprod()
+	subroutine rankrankprod(A, B)
+		! A*B = C - mat A en B allebei niet van volle rang
+		! aangezien we de getransponeerden opslaan: (C^t = C%Vt^t*C%Ut)
+		! C^t = (A*B)^t = B^t*A^t = (B%Ut^t*B%Vt)^t*(A%Ut^t*A%Vt)^t
+		!						  = B%Vt^t*B%Ut*A%Vt^t*A%Ut
+		! we berekenen dus C%Ut = (B%Ut*A%Vt^t)*A%Ut en C%Vt = B%Vt
+
+		type(Matrix), pointer :: A, B, C
+		double precision, dimension(size(B%Ut,1),size(A%Vt,1)) :: tempM
+		double precision :: alpha, beta
+		integer :: m,n,k
+		
+		m = size(B%Ut,1)
+		n = size(A%Vt,1)
+		n2 = size(A%Ut,2)
+		k = size(B%Ut,2)
+		alpha = 1.0
+		beta = 0.0
+
+		allocate(C)
+		C%full = .false.
+		allocate(C%Ut(m,n))
+		allocate(C%Vt(size(A%Ut,1),size(A%Ut,2)))
+		C%Vt = B%Vt
+		
+		!write(*,*) 'DEBUGGING INFORMATION'
+		!write(*,*) 'size of A%Ut: ', size(A%Ut,1), ',', size(A%Ut,2)
+		!write(*,*) 'size of B%Ut: ', size(B%Ut,1), ',', size(B%Ut,2)
+		
+		call dgemm('N','T', m, n, k, alpha, B%Ut, m, A%Vt, n, beta, tempM, m)
+		call dgemm('N','N', m, n2, n, alpha, C, m, A%Ut, n, beta, C%Ut, m)
+
+		call matrixWriter(C)
+		
+		deallocate(C)
+
 	end subroutine
 
 end module
