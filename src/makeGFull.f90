32785f9de977fac87a2d96c303f0eb67cb35e7d2
Dries_DS <dries.ds@hotmail.com>
2015-11-15 13:02:55 +0100

    solveintfull first try

--- src/makeGFull.f90
+++ src/makeGFull.f90
@@ -15,17 +15,28 @@ contains
 		allocate(G)
 		G%full = .true.
 		allocate(G%Ut(N,N))
-		do j = 1,N
-			do i = 1,N
-				! na uitschrijven van de kwadraten en gebruik te maken van 
-				! goniometrische formules.
-				G%Ut(i,j) = -0.5*log( 2*(1-cos((1.0+2*j-2*i)/N*pi)) )
-			enddo
-		enddo
+
+		call calculateG(G%Ut, N)
 
 		call matrixWriter(G)
 
 		deallocate(G)
+
+	end subroutine
+
+	subroutine calculateG(mat, N)
+		double precision(N,N), intent(out) :: mat
+		integer :: i,j
+		double precision, parameter :: pi=4.0*atan(1.0)
+		integer, intent(in) :: N
+
+		do j = 1,N
+			do i = 1,N
+				! na uitschrijven van de kwadraten en gebruik te maken van 
+				! goniometrische formules.
+				mat(i,j) = -0.5*log( 2*(1-cos((1.0+2*j-2*i)/N*pi)) )
+			enddo
+		enddo
 
 	end subroutine
 
--- src/solveIntFull.f90
+++ src/solveIntFull.f90
@@ -1,11 +1,41 @@
 module solveIntFullmod
 
+use makeGFullmod
+use matrixconverter
+
 implicit none
 
 contains
 
-	subroutine solveIntFull()
-		write(*,*) 'in subroutine solveIntFull'
+	subroutine solveIntFull(N)
+		type(Matrix), pointer :: X, U
+		double precision :: A(N:N), B(N), IPIV(N)
+		integer, intent(in) :: N
+		integer :: i, info
+
+		call calculateG(A,N)
+		forall (i=1:N) b(i) = cos(2*i*pi/N)
+		forall (i=1:N) IPIV(i) = (i)
+
+		call dgetrs('N', N, 1, A, N, IPIV, B, N, info)
+
+		call matrixReader(X)
+
+		allocate(U)
+		U%full = .true.
+		allocate(U%Ut(size(X%Ut,1),1))
+
+		U%Ut = 0
+		do i=1,size(U%Ut,1)
+			do j=1,N
+				U%Ut(i) = U%Ut(i)-B(j)*1.0/2*log(1+pow(X%Ut(j,1),2)+pow(X%Ut(j,2),2)-2*(X%Ut(j,1)*cos((2*j+1)*pi/N)+X%Ut(j,2)*sin((2*j+1)*pi/N)))
+			enddo
+		enddo
+
+		call matrixWriter(U)
+
+		deallocate(X,U)
+
 	end subroutine
 
 end module
