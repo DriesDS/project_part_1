dce501134bf54488f058a27197ae2e0f8b94ee6b
Dries_DS <dries.ds@hotmail.com>
2015-11-15 11:03:30 +0100

    make full G first try

--- src/makeGFull.f90
+++ src/makeGFull.f90
@@ -1,12 +1,35 @@
 module makeGFullmod
 
+use matrixconverter
+
 implicit none
 
 contains
 
-	subroutine makeGFull()
-		write(*,*) 'in subroutine makeGFull'
+	subroutine makeGFull(N)
+		type(Matrix) :: G
+		integer :: i,j
+		integer, intent(in) :: N
+		double precision :: pi
+
+		pi = 4.0*atan(1.0)
+
+		allocate(G)
+		G%full = .true.
+		allocate(G%Ut(N,N))
+
+		do j = 1,N
+			do i = 1,N
+				! na uitschrijven van de kwadraten en gebruik te maken van 
+				! goniometrische formules.
+				G%Ut(i,j) = -0.5*log( 2*(1-cos((1+2*j-2*i)/N*pi)) )
+			enddo
+		enddo
+
+		call matrixWriter(G)
+
+		deallocate(G)
+
 	end subroutine
 
 end module
-
