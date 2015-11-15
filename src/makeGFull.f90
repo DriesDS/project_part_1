1d4bdbd3b4568959e301c957a25c8a9793c018c6
Dries De Samblanx <r0384780@zwalm.cs.kotnet.kuleuven.be>
2015-11-15 15:59:12 +0100

    result of makeGFull 40 added

--- src/makeGFull.f90
+++ src/makeGFull.f90
@@ -10,7 +10,7 @@ contains
 		type(Matrix), pointer :: G
 		integer :: i,j
 		integer, intent(in) :: N
-		double precision, parameter :: pi=4.0*atan(1.0)
+		double precision, parameter :: pi=4d0*atan(1d0)
 
 		allocate(G)
 		G%full = .true.
@@ -27,20 +27,14 @@ contains
 	subroutine calculateG(mat, N)
 		double precision, intent(inout) :: mat(N,N)
 		integer :: i,j
-		double precision, parameter :: pi=4.0*atan(1.0)
+		double precision, parameter :: pi=4d0*atan(1d0)
 		integer, intent(in) :: N
-		write(*,*) 'calculating G with N= ', N
-		write(*,*) 'size matrix mat: ', size(mat,1), size(mat,2)
+
 		do j = 1,N
 			do i = 1,N
-				write(*,*) 'j=',j,'  i=',i
-				! na uitschrijven van de kwadraten en gebruik te maken van 
-				! goniometrische formules.
-				write(*,*) cos((1.0+2*j-2*i)/N*pi)
 				mat(i,j) = -0.5*log( 2*(1-cos((1.0+2*j-2*i)/N*pi)) )
 			enddo
 		enddo
-		write(*,*) 'G calculated', mat
 	end subroutine
 
 end module
--- src/solveIntFull.f90
+++ src/solveIntFull.f90
@@ -8,31 +8,29 @@ implicit none
 contains
 
 	subroutine solveIntFull(N)
-		type(Matrix), pointer :: X, U
-		double precision :: A(N:N), B(N), IPIV(N)
-		double precision, parameter :: pi=4.0*atan(1.0)
 		integer, intent(in) :: N
+		type(Matrix), pointer :: X, U
+		double precision :: A(N,N), B(N,1), IPIV(N)
+		double precision, parameter :: pi=4d0*atan(1d0)
 		integer :: i, j, info
-		write(*,*) 'calculating G'
-		A = 0
 		call calculateG(A,N)
-		write(*,*) 'G calculated'
-		forall (i=1:N) b(i) = cos(2*i*pi/N)
+		forall (i=1:N) B(i,1) = cos(2*i*pi/N)
 		forall (i=1:N) IPIV(i) = (i)
-		write(*,*) 'b and ipiv set'
+		write(*,*) A
+		write(*,*) B
+		call dgetrf(N,N,A,N,IPIV,info)
 		call dgetrs('N', N, 1, A, N, IPIV, B, N, info)
 		write(*,*) B
-
 		call matrixReader(X)
 
 		allocate(U)
 		U%full = .true.
-		allocate(U%Ut(size(X%Ut,1),1))
+		allocate(U%Ut(size(X%Ut,2),1))
 
 		U%Ut = 0
-		do i=1,size(U%Ut,1)
+		do i=1,size(U%Ut,2)
 			do j=1,N
-				U%Ut(i,1) = U%Ut(i,1)-B(j)*1.0/2*log(1+X%Ut(j,1)**2+X%Ut(j,2)**2-2*(X%Ut(j,1)*cos((2*j+1)*pi/N)+X%Ut(j,2)*sin((2*j+1)*pi/N)))
+				U%Ut(i,1) = U%Ut(i,1)-B(j,1)*1.0/2*log(1+X%Ut(j,1)**2+X%Ut(j,2)**2-2*(X%Ut(j,1)*cos((2*j+1)*pi/N)+X%Ut(j,2)*sin((2*j+1)*pi/N)))
 			enddo
 		enddo
 
