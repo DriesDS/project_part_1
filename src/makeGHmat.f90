380af15de04f9d2085e53b5f054ca64759a364a5
Dries_DS <dries.ds@hotmail.com>
2015-12-30 15:43:47 +0100

    test

--- src/makeGHmat.f90
+++ src/makeGHmat.f90
@@ -62,34 +62,34 @@ contains
 		if (Dist>s*gamma) then
 			GH%subtypeH = .false.
 
-			!!! uncomment for approximation by formula
-			allocate(GH%endmat)
-			GH%endmat%full = .false.
-			GH%endmat%pointU = .false.
-			GH%endmat%pointV = .false.
-			allocate(GH%endmat%Ut(k,s),GH%endmat%Vt(k,s))
-			call calculateGapprox(GH%endmat, N, beginx, beginy, s)
-			!!! \uncomment for approximation by formula
-
-! 			!!! uncomment for approximation by svd
-! 			allocate(fullapprox)
+! 			!!! uncomment for approximation by formula
 ! 			allocate(GH%endmat)
-! 			fullapprox%full = .true.
-! 			fullapprox%pointU = .false.
 ! 			GH%endmat%full = .false.
 ! 			GH%endmat%pointU = .false.
 ! 			GH%endmat%pointV = .false.
-! 			allocate(fullapprox%Ut(s,s))
 ! 			allocate(GH%endmat%Ut(k,s),GH%endmat%Vt(k,s))
+! 			call calculateGapprox(GH%endmat, N, beginx, beginy, s)
+! 			!!! \uncomment for approximation by formula
 
-! 			call calculateGt(fullapprox%Ut, N, beginx, beginy, s)
-! 			call lowrank(fullapprox, rankapprox, rank=k)
+			!!! uncomment for approximation by svd
+			allocate(fullapprox)
+			allocate(GH%endmat)
+			fullapprox%full = .true.
+			fullapprox%pointU = .false.
+			GH%endmat%full = .false.
+			GH%endmat%pointU = .false.
+			GH%endmat%pointV = .false.
+			allocate(fullapprox%Ut(s,s))
+			allocate(GH%endmat%Ut(k,s),GH%endmat%Vt(k,s))
 
-! 			GH%endmat%Ut(1:k, 1:s) = rankapprox%Ut(1:k, 1:s)
-! 			GH%endmat%Vt(1:k, 1:s) = rankapprox%vt(1:k, 1:s)
-! 			call M_dealloc(rankapprox)
-! 			call M_dealloc(fullapprox)
-! 			!!! \uncomment for approximation by svd
+			call calculateGt(fullapprox%Ut, N, beginx, beginy, s)
+			call lowrank(fullapprox, rankapprox, rank=k)
+
+			GH%endmat%Ut(1:k, 1:s) = rankapprox%Ut(1:k, 1:s)
+			GH%endmat%Vt(1:k, 1:s) = rankapprox%vt(1:k, 1:s)
+			call M_dealloc(rankapprox)
+			call M_dealloc(fullapprox)
+			!!! \uncomment for approximation by svd
 
 			return
 		endif
